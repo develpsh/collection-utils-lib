26f26a320744fc0286a7fe19496223c15488f7c3
kk920106 <kk920106@naver.com>
2024-12-13 22:56:13 +0900

    PgPaymentServiceImpl createPayment 라이브러리 테스트

--- src/main/java/com/pgeasy/www/PgPaymentService.java
+++ src/main/java/com/pgeasy/www/PgPaymentService.java
@@ -5,5 +5,5 @@ import org.json.simple.JSONObject;
 public interface PgPaymentService {
 //    JSONObject createPayment(PgEasyCreatePaymentRequest pgEasyCreatePaymentRequest, String secretKey) throws Exception;
 //    JSONObject createPayment(Object object, PaymentCompany paymentCompany, String secretKey) throws Exception;
-    JSONObject createPayment(JSONObject jsonObject, String secretKey, PaymentCompany paymentCompany) throws Exception;
+    JSONObject createPayment(JSONObject jsonObject, String secretKey, PaymentCompany paymentCompany);
 }
