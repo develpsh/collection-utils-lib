0122ad4f239fc674302a81a6c8bfb42b4e354fce
kk920106 <kk920106@naver.com>
2024-12-14 10:47:14 +0900

    PgPaymentServiceImpl createPayment 라이브러리 테스트

--- src/main/java/com/pgeasy/www/PgPaymentService.java
+++ src/main/java/com/pgeasy/www/PgPaymentService.java
@@ -3,7 +3,6 @@ package com.pgeasy.www;
 import org.json.simple.JSONObject;
 
 public interface PgPaymentService {
-//    JSONObject createPayment(PgEasyCreatePaymentRequest pgEasyCreatePaymentRequest, String secretKey) throws Exception;
-//    JSONObject createPayment(Object object, PaymentCompany paymentCompany, String secretKey) throws Exception;
-    JSONObject createPayment(JSONObject jsonObject, String secretKey, PaymentCompany paymentCompany);
+    JSONObject createModule(JSONObject jsonObject, String secretKey, PaymentCompany paymentCompany);
+    JSONObject approvePayment(JSONObject jsonObject, String secretKey, PaymentCompany paymentCompany);
 }
--- src/main/java/com/pgeasy/www/PgPaymentServiceImpl.java
+++ src/main/java/com/pgeasy/www/PgPaymentServiceImpl.java
@@ -1,8 +1,8 @@
 package com.pgeasy.www;
 
+import lombok.extern.slf4j.Slf4j;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
 
 import java.io.*;
 import java.net.HttpURLConnection;
@@ -10,19 +10,30 @@ import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 
+@Slf4j
 public class PgPaymentServiceImpl implements PgPaymentService {
-    // 결제 생성
-//    public JSONObject createPayment(PgEasyCreatePaymentRequest pgEasyCreatePaymentRequest, String secretKey) throws Exception {
-//    public JSONObject createPayment(Object object, PaymentCompany paymentCompany, Str
-    public JSONObject createPayment(JSONObject jsonObject, String secretKey, PaymentCompany paymentCompany) {
-//        validateCreatePaymentVO(paymentCompany, object);
-        String url = getCreatePaymentUrl(paymentCompany);;
-//        String jsonPayload = new Gson().toJson(jsonObject);
-//        return sendRequest(parseRequestData(jsonPayload), secretKey, url);
+    // 결제 모듈창
+    public JSONObject createModule(JSONObject jsonObject, String secretKey, PaymentCompany paymentCompany) {
+        String url = getCreateModule(paymentCompany);
         return sendRequest(jsonObject, secretKey, url, paymentCompany);
     }
 
-    private String getCreatePaymentUrl(PaymentCompany paymentCompany) {
+    private String getCreateModule(PaymentCompany paymentCompany) {
+        if (PaymentCompany.KAKAO == paymentCompany) {
+            return "https://open-api.kakaopay.com/online/v1/payment/ready";
+        }
+        else {
+            return "";
+        }
+    }
+
+    // 결제 승인
+    public JSONObject approvePayment(JSONObject jsonObject, String secretKey, PaymentCompany paymentCompany) {
+        String url = getApprovePaymentUrl(paymentCompany);
+        return sendRequest(jsonObject, secretKey, url, paymentCompany);
+    }
+
+    private String getApprovePaymentUrl(PaymentCompany paymentCompany) {
         if (PaymentCompany.TOSS == paymentCompany) {
             return "https://api.tosspayments.com/v1/payments/confirm";
         }
@@ -34,52 +45,12 @@ public class PgPaymentServiceImpl implements PgPaymentService {
         }
     }
 
-    private void validateCreatePaymentVO(PaymentCompany paymentCompany, Object object) {
-        if (PaymentCompany.TOSS == paymentCompany) {
-            TossCreatePayment tossCreatePayment = (TossCreatePayment) object;
-            System.out.println(tossCreatePayment);
-        }
-        else if (PaymentCompany.KAKAO == paymentCompany) {
-            KaKaoCreatePayment kaKaoCreatePayment = (KaKaoCreatePayment) object;
-            System.out.println(kaKaoCreatePayment);
-        }
-    }
-
-    // 결제 결과 callback
-    public void handlePaymentCallback() {
-        // Implementation here
-    }
-
-    // 가맹점 승인
-    public void approveMerchant() {
-        // Implementation here
-    }
-
-    // 결제 환불
-    public void refundPayment() {
-        // Implementation here
-    }
-
-    // 결제 상태 확인
-    public PaymentStatus checkPaymentStatus() {
-        // Implementation here
-        return null;
-    }
-
-    private JSONObject parseRequestData(String jsonBody) {
-        try {
-            return (JSONObject) new JSONParser().parse(jsonBody);
-        } catch (ParseException e) {
-//            log.error("JSON Parsing Error", e);
-            return new JSONObject();
-        }
-    }
-
     private JSONObject sendRequest(JSONObject requestData, String secretKey, String urlString, PaymentCompany paymentCompany)  {
         HttpURLConnection connection = createConnection(secretKey, urlString, paymentCompany);
         try (OutputStream os = connection.getOutputStream()) {
             os.write(requestData.toString().getBytes(StandardCharsets.UTF_8));
         } catch (IOException e) {
+            log.error("Error write response", e);
             JSONObject errorResponse = new JSONObject();
             errorResponse.put("error", "Error write response");
             return errorResponse;
@@ -89,7 +60,7 @@ public class PgPaymentServiceImpl implements PgPaymentService {
              Reader reader = new InputStreamReader(responseStream, StandardCharsets.UTF_8)) {
             return (JSONObject) new JSONParser().parse(reader);
         } catch (Exception e) {
-//            log.error("Error reading response", e);
+            log.error("Error reading response", e);
             JSONObject errorResponse = new JSONObject();
             errorResponse.put("error", "Error reading response");
             return errorResponse;
@@ -121,4 +92,28 @@ public class PgPaymentServiceImpl implements PgPaymentService {
             return "";
         }
     }
+
+/*
+
+    // 결제 결과 callback
+    public void handlePaymentCallback() {
+        // Implementation here
+    }
+
+    // 가맹점 승인
+    public void approveMerchant() {
+        // Implementation here
+    }
+
+    // 결제 환불
+    public void refundPayment() {
+        // Implementation here
+    }
+
+    // 결제 상태 확인
+    public PaymentStatus checkPaymentStatus() {
+        // Implementation here
+        return null;
+    }
+*/
 }
